35da7fb1c5dbf7296a75b9bdbf8c69a241e583f9
Nicolás Ibarra <nicoibarra@Nicolass-MacBook-Pro.local>
2021-10-31 19:08:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
+var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+app.use(cors());
+app.use(formidable());
+
+app.post("/form", async (req, res) => {
+  try {
+    const data = {
+      from: `${req.fields.firstname} ${req.fields.lastname}  <nicolito@soymuybello.com>`,
+      to: `nicolas.a.ibarra.g@gmail.com`,
+      subject: "Thank you for sent the email",
+      text: `${req.fields.message}`,
+    };
+
+    mailgun.messages().send(data, (error, body) => {
+      console.log(body);
+
+      if (error === undefined)
+        res.json({
+          Message:
+            "All the information was well recieved and the email was sent.",
+        });
+      else {
+        res.json(error);
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ Error: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server has started");
+});
